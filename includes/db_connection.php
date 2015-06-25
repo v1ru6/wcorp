68b7eb4598960b9e6fa68aaee1c08a3d968ffa76
Vlad Pandichi <vladz0r9@yahoo.com>
2015-06-25 19:17:00 +0300

    REMOVED SENSITIVE INFO

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,15 +1,9 @@
 <?php  
 	// Creating a DB connection
-	define("DB_SERVER", "localhost");
-	define("DB_USER", "root");
-	define("DB_PASS", "candrbrsn11TT");
-	define("DB_NAME", "widget_corp");
-	// WE DEFINED CONSTANTS ABOVE SO NO NEED TO USE THE BELOW VARIABLES
-		// $dbhost = "localhost";
-		// $dbuser = "root";
-		// $dbpass = "candrbrsn11TT";
-	// 	// $dbname = "widget_corp";
-	// $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+	define("DB_SERVER", "/");
+	define("DB_USER", "/");
+	define("DB_PASS", "/");
+	define("DB_NAME", "/");
 	$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME); // this is considered better practice
 	// test if successful
 	if(mysqli_connect_errno()) {
